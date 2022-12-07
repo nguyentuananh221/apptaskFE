181a526edc7a10c892f46d00b07a1c4ecd00dbbd
tuananh <anhpro12anh@gmail.com>
2022-12-07 23:06:20 +0700

    fix

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://apptaskmana.onrender.com/api'
+const baseUrl = 'https://apptaskbe.onrender.com/api'
 const api = {
     call : () => {
         return axios.create({
